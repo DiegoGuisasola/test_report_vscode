7e516d2528dec113ab64612e3435dc034683e557
Diego Guisasola <diego.guisasola@utp.edu.co>
2021-04-06 04:55:11 -0500

    Initial commit

---
+++ test1.py
@@ -0,0 +1,26 @@
+import math
+import os
+import sys
+
+import requests
+
+print(sys.version)
+print(sys.executable)
+
+# # Check if inside a venv
+# def is_venv():
+#     return hasattr(sys, "real_prefix") or (
+#         hasattr(sys, "base_prefix") and sys.base_prefix != sys.prefix
+#     )
+
+print(x)
+test = 2
+
+
+# if is_venv():
+#     print("inside virtualenv or venv")
+# else:
+#     print("outside virtualenv or venv")
+# # Requests module
+# r = requests.get("https://coreyms.com")
+# print(r.status_code)
