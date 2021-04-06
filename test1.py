766f8b14cc89d23bfa8c8bd90e48e4a76be64860
Diego Guisasola <diego.guisasola@utp.edu.co>
2021-04-06 05:53:24 -0500

    code deleted

--- test1.py
+++ test1.py
@@ -1,19 +1,5 @@
 import requests
 
-# print(sys.version)
-# print(sys.executable)
-
-# # Check if inside a venv
-# def is_venv():
-#     return hasattr(sys, "real_prefix") or (
-#         hasattr(sys, "base_prefix") and sys.base_prefix != sys.prefix
-#     )
-
-if is_venv():
-    print("inside virtualenv or venv")
-else:
-    print("outside virtualenv or venv")
-
-# Requests module
 r = requests.get("https://coreyms.com")
 print(r.status_code)
+print(r.ok)
